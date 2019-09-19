d2e7072364f706ec344470224448157eb409632e
Fabio <fabio.junior@uotz.com.br>
2019-09-19 18:39:43 -0300

    Criação dos smock tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const test = () => console.log('aki');
-
-test();
+export const search = () => {};
+export const searchAlbums = () => {};
+export const searchArtists = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,26 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/index';
 
-describe('Inicial Test', () => {
-  it('should return 5', () => {
-    expect(3 + 3).to.equal(6);
+describe('Spotify Wrapper', () => {
+  context('smock tests', () => {
+    it('method search should be exist', () => {
+      expect(search).to.exist;
+    });
+
+    it('method searchAlbums should be exist', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('method searchArtists should be exist', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('method searchTracks should be exist', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('method searchPlaylists should be exist', () => {
+      expect(searchPlaylists).to.exist;
+    });
   });
 });
